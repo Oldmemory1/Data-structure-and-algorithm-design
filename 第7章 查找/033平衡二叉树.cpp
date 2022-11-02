8348af908484cae7d6ea2e8913fd364f70755fde
Oldmemory1 <94025104+Oldmemory1@users.noreply.github.com>
2022-11-02 20:53:58 +0800

    Update 033平衡二叉树.cpp

--- 第7章 查找/033平衡二叉树.cpp
+++ 第7章 查找/033平衡二叉树.cpp
@@ -29,29 +29,29 @@ x
 #include <bits/stdc++.h>
 typedef struct BiTNode{
     char data;
-    struct BiTNode *lchild,*rchild;
+    struct BiTNode *LeftChild,*RightChild;
 }BiTNode,*BiTree;
 
 void PreOrder(BiTree T){  //DLR 先序输出
     if(T){
         printf("%c",T->data);
-        PreOrder(T->lchild);
-        PreOrder(T->rchild);
+        PreOrder(T->LeftChild);
+        PreOrder(T->RightChild);
     }
 }
 
 void InOrder(BiTree T){   //LDR 中序输出
     if(T){
-        InOrder(T->lchild);
+        InOrder(T->LeftChild);
         printf("%c",T->data);
-        InOrder(T->rchild);
+        InOrder(T->RightChild);
     }
 }
 
 void PostOrder(BiTree T){   //LRD 后序输出
     if(T){
-        PostOrder(T->lchild);
-        PostOrder(T->rchild);
+        PostOrder(T->LeftChild);
+        PostOrder(T->RightChild);
         printf("%c",T->data);
     }
 }
@@ -60,45 +60,45 @@ int height(BiTree T){       //返回左右子树中 高度较大的高度
     int leftheight,rightheight;
     if(T==NULL)return 0;
     else{
-        leftheight=height(T->lchild);
-        rightheight=height(T->rchild);
+        leftheight=height(T->LeftChild);
+        rightheight=height(T->RightChild);
         return (leftheight>rightheight?leftheight+1:rightheight+1);
     }
 }
 
 BiTree singleRotationLL(BiTree T){  //LL 向右转
-    BiTree A=T->lchild;  //根=左
-    T->lchild=A->rchild;
-    A->rchild=T;
+    BiTree A=T->LeftChild;  //根=左
+    T->LeftChild=A->RightChild;
+    A->RightChild=T;
     return A;
 }
 
 BiTree singleRotationRR(BiTree T){  //RR 向左转
-    BiTree A=T->rchild;
-    T->rchild=A->lchild;
-    A->lchild=T;
+    BiTree A=T->RightChild;
+    T->RightChild=A->LeftChild;
+    A->LeftChild=T;
     return A;
 }
 
 BiTree doubleRotationLR(BiTree T3){  //LR 孩子左转、右转
     BiTree T2,T1;
-    T1=T3->lchild;
-    T2=T1->rchild;
-    T1->rchild=T2->lchild;
-    T3->lchild=T2->rchild;
-    T2->lchild=T1;
-    T2->rchild=T3;
+    T1=T3->LeftChild;
+    T2=T1->RightChild;
+    T1->RightChild=T2->LeftChild;
+    T3->LeftChild=T2->RightChild;
+    T2->LeftChild=T1;
+    T2->RightChild=T3;
     return T2;
 }
 
 BiTree doubleRotationRL(BiTree T1){   //RL 孩子右转、左转
     BiTree T2,T3;
-    T3=T1->rchild;
-    T2=T3->lchild;
-    T1->rchild=T2->lchild;
-    T3->lchild=T2->rchild;
-    T2->lchild=T1;
-    T2->rchild=T3;
+    T3=T1->RightChild;
+    T2=T3->LeftChild;
+    T1->RightChild=T2->LeftChild;
+    T3->LeftChild=T2->RightChild;
+    T2->LeftChild=T1;
+    T2->RightChild=T3;
     return T2;
 }
 
@@ -106,20 +106,20 @@ BiTree insert(char c,BiTree T){
     if(T==NULL){
         T=(BiTree)malloc(sizeof(BiTNode));
         T->data=c;
-        T->lchild=T->rchild=NULL;
+        T->LeftChild=T->RightChild=NULL;
     }
     else{
         if(c<T->data){
-            T->lchild=insert(c,T->lchild);
-            if(height(T->lchild)-height(T->rchild)==2){
-                if(c<T->lchild->data)T=singleRotationLL(T);
-                else T=singleRotationLR(T);
+            T->LeftChild=insert(c,T->LeftChild);
+            if(height(T->LeftChild)-height(T->RightChild)==2){
+                if(c<T->LeftChild->data)T=singleRotationLL(T);
+                else T=doubleRotationLR(T);
             }
         }
         else if(c>T->data){
-            T->rchild=insert(c,T->rchild);
-            if(height(T->rchild)-hieght(T->lchild)==2){
-                if(c>T->rchild->data)T=singleRotationRR(T);
+            T->RightChild=insert(c,T->RightChild);
+            if(height(T->RightChild)-height(T->LeftChild)==2){
+                if(c>T->RightChild->data)T=singleRotationRR(T);
                 else T=doubleRotationRL(T);
             }
         }
@@ -128,10 +128,10 @@ BiTree insert(char c,BiTree T){
 }
 
 void turnTree90DegreeLeft(BiTree T,int depth){    //右根左
-    if(T->rchild)turnTree90DegreeLeft(T->rchild,depth+1);  
+    if(T->RightChild)turnTree90DegreeLeft(T->RightChild,depth+1);  
     for(int i=1;i<depth;i++)printf("    ");
     printf("%c\n",T->data);
-    if(T->lchild)turnTree90DegreeLeft(T->lchild,depth+1);
+    if(T->LeftChild)turnTree90DegreeLeft(T->LeftChild,depth+1);
 }
 
 BiTree root=NULL;
@@ -142,11 +142,6 @@ int main(){
     printf("Preorder: "); PreOrder(root); printf("\n");
     printf("Inorder: "); InOrder(root); printf("\n");
     printf("Postorder: "); PostOrder(root); printf("\n");
-    printf("Tree: \n"); turnTree90DegreeLeft(root,1);
+    printf("Tree:\n"); turnTree90DegreeLeft(root,1);
     return 0;
 }
-
-
-
-
-
